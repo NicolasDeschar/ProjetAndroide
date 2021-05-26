afeeb189ea50e7d1fc507349efcb66503cedbc21
AlixZh <72941464+AlixZh@users.noreply.github.com>
2021-05-26 18:27:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#exemple de tests pour obtenir un environnement contenant des cubes
+
 import pybullet 
 import pybullet_data
 import time
@@ -5,48 +7,51 @@ import numpy as np
 import sys
 import create_file
 
-W=100
+#taille de l environnement
+W=100 
 H=100
 L=10
 
 
-def posDiff(pos,list_pos):
+def posDiff(pos,list_pos): #pour obtenir des positions de cubes espaces
     for i in list_pos:
         if(abs(pos[0]-i[0])<1.5 and abs(pos[1]-i[1])<1.5):
             return False
     return True
 
-def my_setJointMotorControlMultiDof(robot_id):
+def my_setJointMotorControlMultiDof(robot_id): #creer 
     for i in robot_id:
         pybullet.setJointMotorControlMultiDof(bodyUniqueId=i,
             jointIndex=0,
             controlMode=pybullet.POSITION_CONTROL,
             targetPosition=pybullet.getQuaternionFromEuler((np.random.rand(3)*np.pi).tolist()),
-            #targetPosition=pybullet.getQuaternionFromEuler([2,3,4]),
             force=[10,10,10])
 
+#creation de l environnement
 create_file.create_plane(W,L,H)        
 pybullet.connect(pybullet.GUI)
 pybullet.setGravity(0,0,-10)
 #pybullet.loadURDF(pybullet_data.getDataPath()+"/plane.urdf", [0, 0, 0])
 pybullet.loadURDF("plane/plane.urdf",[0,0,0])
-# constant h : int
+# constante h : int
 h=2
 nb_cube=10
-#cube positions
 
+#cube positions
 cubes_pos=[]
 while(len(cubes_pos)<nb_cube):
     pos= [np.random.randint(-10,10,1),np.random.randint(-10,10,1),h]
     if(posDiff(pos,cubes_pos)):
 	    cubes_pos.append(pos)
-
+	
+#creation des cubes
 urdf_names=[]
 nameObj=create_file.create() #create all the 
 for i in nameObj:
     urdf_names.append("cubes"+chr(47)+str(i)+"_"+str(nameObj[i][0]))
 
 robot_id=[]
+#creer un referentiel pour chaque cube, pour qu il reste en hauteur sans tomber de l environnement
 for i in range(nb_cube):
     robot_id.append(pybullet.loadURDF(urdf_names[i]+".urdf"))
     #robot_id.append(pybullet.loadURDF("cube_individual.urdf"))
